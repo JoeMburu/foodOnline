5ec0ceeb11aebae2ba1ff939d5ea0abcf90a6ac4
Joseph Afonne <josef.afonne@gmail.com>
2025-12-04 12:29:05 +0200

    Vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+
+from vendor.forms import VendorRegistrationForm
 from .forms import UserRegistrationForm
 from .models import User
 from django.contrib import messages
@@ -15,6 +17,7 @@ def registerUser(request):
             # You can add a redirect or success message here
             return redirect('registerUser')  # Redirect to the same page or another page
         else:
+            messages.error(request, 'Please correct the errors below.')
             return render(request, 'accounts/register_user.html', {'form': form})
 
     form = UserRegistrationForm()
@@ -23,5 +26,35 @@ def registerUser(request):
     }
     return render(request, 'accounts/register_user.html', context)
 
-def registerRestaurant(request):
-    pass
+def registerVendor(request):
+    if request.method == 'POST':
+        # store data and create user
+        form = UserRegistrationForm(request.POST)
+        vendor_form = VendorRegistrationForm(request.POST, request.FILES)
+        if form.is_valid() and vendor_form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.VENDOR
+            user.save()
+
+            vendor = vendor_form.save(commit=False)
+            vendor.user = user
+            vendor.user_profile = user.userprofile
+            vendor.save()
+
+            messages.success(request, 'Your vendor account has been registered successfully! Please wait for approval.')
+            return redirect('registerVendor')  # Redirect to the same page or another page
+        else:
+            print("INVALID FORM")
+            messages.error(request, 'Please correct the errors below.')
+            return render(request, 'accounts/register_vendor.html', {'form': form, 'vendor_form': vendor_form})
+    else:
+        form = UserRegistrationForm()
+        vendor_form = VendorRegistrationForm()
+        context = {
+            'form': form,
+            'vendor_form': vendor_form
+        }
+        return render(request, 'accounts/register_vendor.html', context)
+
+        
+    
--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'foodonline_main',
+    'vendor',
 ]
 
 MIDDLEWARE = [
