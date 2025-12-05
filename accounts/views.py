6031628545bd8538ba7261b763d05470263a8f72
Joseph Afonne <josef.afonne@gmail.com>
2025-12-05 15:00:18 +0200

    implement user authentication and authorization

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 
+from accounts.utils import detectUser
 from vendor.forms import VendorRegistrationForm
 from .forms import UserRegistrationForm
 from .models import User
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
+
+
+
+# authorization methods
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+    
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied  
 
 # Create your views here.
 def registerUser(request):
@@ -65,7 +83,7 @@ def registerVendor(request):
 def login(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in.')
-        return redirect('dashboard')  # Redirect to a dashboard or home page if already logged in
+        return redirect('myAccount')  # Redirect to a dashboard or home page if already logged in
     elif request.method == 'POST':
         # Authenticate user
         email = request.POST.get('email') # request.Post['email']
@@ -75,7 +93,7 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')  # Redirect to a dashboard or home page 
+            return redirect('myAccount')  # Redirect to a dashboard or home page 
         else:
             messages.error(request, 'Invalid login credentials.')
             return redirect('login')  # Redirect back to login page
@@ -87,6 +105,19 @@ def logout(request):
     messages.info(request, 'You have been logged out.')        
     return redirect('login')
 
-def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
-    
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def customerDashboard(request):
+    return render(request, 'accounts/customerDashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request, 'accounts/vendorDashboard.html')
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    getUserUrl = detectUser(user)
+    return redirect(getUserUrl)
+     
